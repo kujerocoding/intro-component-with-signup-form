81042611191fb41880f4f14953e6cb6186b5ad97
herom <jerome.manset22@gmail.com>
2022-11-30 02:31:17 +0800

    Add script to show icon error

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+// get array of input div
+// get array of input
+
+
 const divFirstName = document.querySelector('.div--firstname');
 const firstName = document.querySelector('.input__firstname');
-const formSignUp = document.querySelector('.form__signup');
 const para = document.createElement("p");
+const iconError = document.querySelector('.icon--error');
 
 
 function showErrorMessage() {
     if (!firstName.value) {
         para.textContent = "First Name cannot be empty"
         para.classList.add('message--error');
+        firstName.classList.add('input--error--border');
         divFirstName.appendChild(para);
+        iconError.style.display = "block";
     } else {
         para.textContent = "";
+        firstName.classList.remove('input--error--border');
+        iconError.style.display = "none";
     }
 }
 

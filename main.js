d645f6665e202d9312573e117b3479dd7c2bf763
herom <jerome.manset22@gmail.com>
2022-11-30 15:42:00 +0800

    Add script for invalid email input

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-// get array of input div
+
+//input array = inputArray[index], div array = inputArray.childer[index]
 const inputArray = document.querySelector('.form__signup');
-
-
-//replace array of input div
-/* const divFirstName = document.querySelector('.div--firstname'); */
-//replace array of input
-/* const firstName = document.querySelector('.input__firstname'); */
 const inputItem = ["First Name", "Last Name", "Email", "Password"];
 const paraError = document.createElement("p");
 const iconError = document.createElement("img");
+
+// SCRIPT FOR EMPTY INPUT
 
 function createIconError () {
     iconError.src = "/images/icon-error.svg";
     iconError.classList.add('icon--error');
 }
-
 
 for (let i = 0; i < inputArray.length - 1; i++) {
 
@@ -42,3 +38,21 @@ for (let i = 0; i < inputArray.length - 1; i++) {
    inputArray[i].addEventListener('input',showErrorMessage);
 
 }
+
+// SCRIPT FOR INVALID EMAIL INPUT
+
+const inputEmail = inputArray[2];
+function notEmail () {
+     if (inputEmail.value) {
+        const regexMatch = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value);
+        if (!regexMatch) {
+            paraError.textContent = "Looks like this is not an email";
+            paraError.classList.add('message--error');
+            inputArray.children[2].appendChild(paraError);
+        } else {
+            paraError.textContent="";
+        }
+    }
+}
+
+inputEmail.addEventListener('input', notEmail);

dfcae2029599426c91b393237a0ac720ac625e68
herom <jerome.manset22@gmail.com>
2022-11-30 14:41:17 +0800

    Add script for all input items when input value is empty

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 // get array of input div
-// get array of input
+const inputArray = document.querySelector('.form__signup');
 
 
+//replace array of input div
 const divFirstName = document.querySelector('.div--firstname');
+//replace array of input
 const firstName = document.querySelector('.input__firstname');
 const para = document.createElement("p");
 const iconError = document.querySelector('.icon--error');
+const inputItem = ["First Name", "Last Name", "Email", "Password"];
 
+for (let i = 0; i < inputArray.length - 1; i++) {
 
-function showErrorMessage() {
-    if (!firstName.value) {
-        para.textContent = "First Name cannot be empty"
-        para.classList.add('message--error');
-        firstName.classList.add('input--error--border');
-        divFirstName.appendChild(para);
-        iconError.style.display = "block";
-    } else {
-        para.textContent = "";
-        firstName.classList.remove('input--error--border');
-        iconError.style.display = "none";
-    }
+    function showErrorMessage() {
+        
+           if (!inputArray[i].value) {
+               
+               para.textContent = `${inputItem[i]} cannot be empty`;
+               para.classList.add('message--error');
+               inputArray[i].classList.add('input--error--border');
+               inputArray.children[i].appendChild(para);
+
+               /* iconError.style.display = "block"; */
+           } else {
+               para.textContent = "";
+               inputArray[i].classList.remove('input--error--border');
+               /* iconError.style.display = "none"; */
+           }
+       
+   }
+
+   inputArray[i].addEventListener('input',showErrorMessage );
+
 }
 
-
-firstName.addEventListener('input',showErrorMessage );
 
 
 /* function createPara () {
